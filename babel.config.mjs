620f280d92509ed906df30d4e32c1674f1e9840a
tarasov.d <d.tarasov@softmedialab.com>
2024-08-20 20:08:28 +0700

    del comments

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,11 +1,3 @@
-// export default {
-//   presets: [
-//     '@babel/preset-env',
-//     ['@babel/preset-react', { runtime: 'automatic' }],
-//     '@babel/preset-typescript',
-//   ],
-// };
-
 export default {
   presets: [
     '@babel/preset-env',
